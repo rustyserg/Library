631de73ae14aa7637bb7c9abb572aabcd605fb9c
rustyserg <rusty_serg@i.ua>
2018-05-30 21:45:51 +0300

    Book cpp and h added

--- Library/Book.cpp
+++ Library/Book.cpp
@@ -1,47 +1,28 @@
 #include "stdafx.h"
 #include "Book.h"
-
+#include <iostream>
 
 Book::Book()
 {
 }
 
-void Book::ViewBook()
+void Book::NewBook()
 {
-	if (list.size() > 0) {
-		for (int i = 0; i < list.size(); i++)
-		{
-			std::cout << i+1 << ". " << list[i].book_title << std::endl;
-			std::cout << "   " << list[i].author_name << std::endl;
-			std::cout << "   " << list[i].year << std::endl;
-		}
-	}
-	else {
-		std::cout << "There is no books in the library" << std::endl;
-	}	
-}
-
-void Book::AddBook()
-{
+	std::cout << "Enter id of the book" << std::endl;
+	std::cin >> book_id;
 	std::cout << "Enter the title of the book" << std::endl;
 	std::cin >> book_title;
 	std::cout << "Enter the author name of the book" << std::endl;
 	std::cin >> author_name;
 	std::cout << "Enter the year of the book" << std::endl;
 	std::cin >> year;
-
-	Book book;
-	book.book_title = book_title;
-	book.author_name = author_name;
-	book.year = year;
-	list.push_back(book);
-	std::cout << "The book added" << std::endl;
 }
 
-void Book::DeleteBook()
+void Book::ToDelete()
 {
+	std::cout << "Enter id of the book wich you want to delete" << std::endl;
+	std::cin >> book_id;
 }
-
 
 Book::~Book()
 {
--- Library/Book.h
+++ Library/Book.h
@@ -1,20 +1,18 @@
 #pragma once
-#include <iostream>
 #include <string>
 #include <vector>
 
 class Book
 {
-private:
+public:
+	Book();
+	void NewBook();
+	void ToDelete();
+	~Book();
+public:
+	unsigned int book_id;
 	std::string book_title;
 	std::string author_name;
 	unsigned short int year;
-	std::vector <Book> list {};
-public:
-	Book();
-	void ViewBook();
-	void AddBook();
-	void DeleteBook();
-	~Book();
 };
 
